128dc6765296ac446e5cfc43ba683a1990635b3e
J0Var4lM <49831808+w754248A3@users.noreply.github.com>
2024-03-30 15:25:49 +0800

    增加反色

--- ts/src/invert_colors/invert_colors.ts
+++ ts/src/invert_colors/invert_colors.ts
@@ -1,113 +1,90 @@
-const videoPlayer = <HTMLVideoElement>document.getElementById('videoPlayer');
+const video_1 = <HTMLVideoElement>document.getElementById('video_1');
+const video_2 = <HTMLVideoElement>document.getElementById('video_2');
 const startRecording = <HTMLButtonElement>document.getElementById('startRecording');
-const stopRecording = <HTMLButtonElement>document.getElementById('stopRecording');
-const canvas = <HTMLCanvasElement>document.getElementById("canvas");
-const context = canvas.getContext('2d');
 let mediaRecorder: MediaRecorder;
 let recordedBlobs: Array<Blob>;
 
 startRecording.addEventListener('click', () => {
     startRecording.disabled = true;
-    stopRecording.disabled = false;
-    recordedBlobs = [];
+
     navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
         .then(stream => {
 
-            videoPlayer.srcObject = stream;
+            video_1.srcObject = stream;
 
-            videoPlayer.controls = false;
+            video_1.controls = true;
 
-            videoPlayer.muted = true;
+            video_1.muted = true;
 
-            videoPlayer.autoplay = true;
-            setTimeout(()=> F开始执行反色(videoPlayer), 1000);
-            mediaRecorder = new MediaRecorder(stream);
+            video_1.autoplay = true;
 
-            mediaRecorder.ondataavailable = event => {
-                if (event.data && event.data.size > 0) {
-                    //recordedBlobs.push(event.data);
-                }
-            };
+            video_1.addEventListener('loadedmetadata', function () {
+                // 获取视频分辨率
 
+                console.log(video_1.videoWidth, video_1.videoHeight);
 
-            mediaRecorder.onstop = function () {
-
-                stream.getTracks().forEach(v => v.stop());
-                const superBuffer = new Blob(recordedBlobs, { type: 'video/webm' });
+                const canvas_v = F创建画布(video_1.videoWidth, video_1.videoHeight);
 
 
+                video_2.srcObject = canvas_v.canvas.captureStream(30);
 
-                const url = URL.createObjectURL(superBuffer);
+                video_2.controls = true;
 
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = 'recording.webm';  // 设置下载文件的名称
-                document.body.appendChild(a);
-                //a.click();
-                // 清理并移除a元素
-                document.body.removeChild(a);
-                //URL.revokeObjectURL(url);
+                video_2.muted = true;
 
-
-                videoPlayer.controls = true;
-
-                videoPlayer.muted = false;
-                videoPlayer.srcObject = null;
-                videoPlayer.src = url;
-
-                videoPlayer.play();
+                video_2.autoplay = true;
+                window.requestAnimationFrame(()=> drawFrame(canvas_v.canvas, canvas_v.content2d, video_1));
                 
-              
-            };
-            mediaRecorder.start();
+            });
+
+
         })
         .catch(error => {
             console.error('获取屏幕流失败', error);
         });
 });
 
-stopRecording.addEventListener('click', () => {
-    stopRecording.disabled = true;
-    startRecording.disabled = false;
-    mediaRecorder.stop();
+
+function F创建画布(width: number, height: number) {
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    const content2d = canvas.getContext("2d");
+
+    if (content2d === null) {
+        throw new Error("没有2D上下文");
+    }
 
 
-});
 
-
-function F开始执行反色(video: HTMLVideoElement){
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    console.log(canvas.width, canvas.height);
-    setTimeout(()=> drawFrame(video), 1000);
-
+    return {
+        canvas,
+        content2d
+    };
 }
 
 
-function drawFrame(video:HTMLVideoElement) {
+function drawFrame(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D, video: HTMLVideoElement) {
 
-    if(context === null){
+    if (context === null) {
         throw new Error("2d 绘制上下文是NULL");
     }
 
     context.drawImage(video, 0, 0);
-    
-    
+
+
     var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     invertColors(imageData.data);
     context.putImageData(imageData, 0, 0);
-    
-    setTimeout(function () {
-      drawFrame(video);
-    }, 10);
-  }
-  
-  
-  function invertColors(data : Uint8ClampedArray) {
-    for (var i = 0; i < data.length; i+= 4) {
-      data[i] = data[i] ^ 255; // Invert Red
-      data[i+1] = data[i+1] ^ 255; // Invert Green
-      data[i+2] = data[i+2] ^ 255; // Invert Blue
+
+    window.requestAnimationFrame(()=> drawFrame(canvas, context, video));
+}
+
+
+function invertColors(data: Uint8ClampedArray) {
+    for (var i = 0; i < data.length; i += 4) {
+        data[i] = data[i] ^ 255; // Invert Red
+        data[i + 1] = data[i + 1] ^ 255; // Invert Green
+        data[i + 2] = data[i + 2] ^ 255; // Invert Blue
     }
-  }
-  
+}
--- ts/webpack.config.js
+++ ts/webpack.config.js
@@ -88,7 +88,8 @@ module.exports = {
   
     entry: {
         //["gen_password/gen_password.js"]: "./src/gen_password/gen_password.ts",
-        ["screen_recording/screen_recording.js"]: "./src/screen_recording/screen_recording.ts",
+        //["screen_recording/screen_recording.js"]: "./src/screen_recording/screen_recording.ts",
+        ["invert_colors/invert_colors.js"]: "./src/invert_colors/invert_colors.ts",
     },
   
     module: {
